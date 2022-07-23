91b158a7bdc2e7e354fecaf8ffc10586077fd741
Sławek Kopytek <slavkopytek@gmail.com>
2022-07-23 14:48:26 +0200

    Add first test

---
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import TripSummary from './TripSummary';
+
+describe('Component TripSummary', () => {
+  it('should render without crashing', () => {
+    const component = shallow(<TripSummary />);
+    expect(component).toBeTruthy();
+  });
+});
